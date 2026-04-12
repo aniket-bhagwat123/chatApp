866adc49483bb0b15df7d267cc7df29b4898652f
aniket <aniketbhagwat95@gmail.com>
2026-04-12 20:36:07 +0530

    nee commits

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import app from './app';
+
+dotenv.config();
+
+// Connection to MongoDB
+connectDB(process.env.MONGO_URI as string);
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
